c3a033ae5427f9a65da1e10522693763ab6d84e3
HUSTjmk <movingon_2023@qq.com>
2025-06-08 21:16:18 +0800

    cotex->m4&lvgl planted

---
+++ drink-maker/Appliaction/Inc/serial_task.h
@@ -0,0 +1,11 @@
+#ifndef SERIAL_TASK_H_
+#define SERIAL_TASK_H_
+
+
+#define SERIAL_TASK_PRIORITY 3
+#define SERIAL_TASK_STACK    4096
+#define SERIAL_MOVE_MASK     0x10
+#define SERIAL_DISPLAY_MASK  0x20
+
+
+#endif
